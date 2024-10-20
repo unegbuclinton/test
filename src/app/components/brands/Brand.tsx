b082da799fcde5cbd8aca07540a5459089877af7
unegbu Clinton <unegbuclinton5@gmail.com>
2024-10-20 22:46:23 +0100

    flex bug

--- src/app/components/brands/Brand.tsx
+++ src/app/components/brands/Brand.tsx
@@ -10,7 +10,7 @@ import Image from 'next/image'
 const Brand = () => {
   const imgs = [google, forbe, bloomberg, sleep, influence]
   return (
-    <div className='flex flex-wrap justify-center md:justify-between lg:flex-nowrap items-center bg-white rounded-sm shadow-shadowLight -mt-8 max-w-[95%] md:max-w-[1276px] py-6 md:py-8 px-4 md:px-16 mx-auto'>
+    <div className='flex flex-wrap justify-center md:justify-between lg:flex-nowrap items-center bg-white rounded-sm shadow-shadowLight -mt-8 max-w-[95%] md:max-w-[1276px] py-6 md:py-8 px-4 md:px-16 mx-auto lg:mx-0 lg:ml-auto'>
       {imgs.map((img, index) => (
         <Image
           className='mr-6 md:mr-12 mb-4 md:mb-0'
